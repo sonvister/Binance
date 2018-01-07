4daa196360fb6db89bc3b8cf40b34e3f93ad75c5
sonvister <sonvister@gmail.com>
2018-01-06 21:20:11 -0600

    Add sample .NET Framework 4.7.1 console app

---
+++ samples/BinanceClassicConsoleApp/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Threading.Tasks;
+using Binance.Api;
+
+namespace BinanceClassicConsoleApp
+{
+    class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            var api = new BinanceApi();
+
+            if (await api.PingAsync())
+                Console.WriteLine("SUCCESSFUL!");
+
+            Console.WriteLine("...press any key to continue.");
+            Console.ReadKey(true);
+        }
+    }
+}
