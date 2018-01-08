2d68dec617159f7282868c2ed9ea1b24d1171924
sonvister <sonvister@gmail.com>
2018-01-08 17:22:15 -0600

    Add sample web applications
    Create minimal ASP.NET (Core and .NET Framework) web applications modifying HomeController to display Binance server time (also remove classic project System.Net.Http 4.0.0.0 reference).

---
+++ samples/BinanceClassicWebApp/Controllers/HomeController.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Mvc;
+using Binance.Api;
+
+namespace BinanceClassicWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<ActionResult> Index()
+        {
+            var api = new BinanceApi();
+
+            var time = await api.GetTimeAsync();
+
+            return Content($"Server Time: {time} UTC");
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ samples/BinanceWebApp/Controllers/HomeController.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using BinanceWebApp.Models;
+using Binance.Api;
+
+namespace BinanceWebApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        public async Task<IActionResult> Index()
+        {
+            var api = new BinanceApi();
+
+            var time = await api.GetTimeAsync();
+
+            return Content($"Server Time: {time} UTC");
+        }
+
+        public IActionResult About()
+        {
+            ViewData["Message"] = "Your application description page.";
+
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            ViewData["Message"] = "Your contact page.";
+
+            return View();
+        }
+
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
