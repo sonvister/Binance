a7d36b71ff3df88cf187308dfe5dfb7bc0804fef
sonvister <sonvister@gmail.com>
2018-01-22 09:54:28 -0600

    Add synchronization to RateLimiter
    Fixes #29.

--- src/Binance/Api/RateLimit/RateLimiter.cs
+++ src/Binance/Api/RateLimit/RateLimiter.cs
@@ -36,6 +36,8 @@ namespace Binance.Api
 
         private int _count;
 
+        private readonly SemaphoreSlim _syncLock = new SemaphoreSlim(1, 1);
+
         private readonly Queue<long> _timestamps = new Queue<long>();
 
         private readonly ILogger<RateLimiter> _logger;
@@ -65,55 +67,68 @@ namespace Binance.Api
             if (_count == 0)
                 return;
 
-            // Create the current timestamp.
-            var now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+            // Acquire synchronization lock.
+            await _syncLock.WaitAsync(token)
+                .ConfigureAwait(false);
 
-            do
+            try
             {
-                // If the maximum count has not been reached.
-                if (_timestamps.Count < _count)
+                // Create the current timestamp.
+                var now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+
+                do
                 {
-                    // Queue the current timestammp.
-                    _timestamps.Enqueue(now);
-                    continue;
-                }
-
-                // Remove the oldest timestamp.
-                var then = _timestamps.Dequeue();
-
-                var millisecondsDelay = 0;
-
-                try
-                {
-                    // How long has it been?
-                    var time = Convert.ToInt32(now - then);
-
-                    // If elapsed time is less than allowed time...
-                    if (time < Duration.TotalMilliseconds)
+                    // If the maximum count has not been reached.
+                    if (_timestamps.Count < _count)
                     {
-                        // ...set the delay as the time difference.
-                        millisecondsDelay = Convert.ToInt32(Duration.TotalMilliseconds) - time;
+                        // Queue the current timestammp.
+                        _timestamps.Enqueue(now);
+                        continue;
                     }
-                }
-                catch (OverflowException) { /* ignore */  }
 
-                // Add the current/future timestammp.
-                _timestamps.Enqueue(now + millisecondsDelay);
+                    // Remove the oldest timestamp.
+                    var then = _timestamps.Dequeue();
 
-                // Delay if required.
-                if (millisecondsDelay <= 0)
-                    continue;
+                    var millisecondsDelay = 0;
 
-                _logger?.LogDebug($"{nameof(RateLimiter)} delaying for {millisecondsDelay} msec.");
+                    try
+                    {
+                        // How long has it been?
+                        var time = Convert.ToInt32(now - then);
 
-                await Task.Delay(millisecondsDelay, token)
-                    .ConfigureAwait(false);
+                        // If elapsed time is less than allowed time...
+                        if (time < Duration.TotalMilliseconds)
+                        {
+                            // ...set the delay as the time difference.
+                            millisecondsDelay = Convert.ToInt32(Duration.TotalMilliseconds) - time;
+                        }
+                    }
+                    catch (OverflowException) { /* ignore */  }
 
-                if (count > 1)
-                {
-                    now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
-                }
-            } while (--count > 0);
+                    // Add the current/future timestammp.
+                    _timestamps.Enqueue(now + millisecondsDelay);
+
+                    // Delay if required.
+                    if (millisecondsDelay <= 0)
+                        continue;
+
+                    _logger?.LogDebug($"{nameof(RateLimiter)} delaying for {millisecondsDelay} msec.");
+
+                    await Task.Delay(millisecondsDelay, token)
+                        .ConfigureAwait(false);
+
+                    if (count > 1)
+                    {
+                        now = DateTimeOffset.UtcNow.ToUnixTimeMilliseconds();
+                    }
+                } while (--count > 0);
+            }
+            catch (Exception) { /* ignore */ }
+            finally
+            {
+                // Release synchronization lock.
+                _syncLock.Release();
+            }
         }
 
         #endregion Public Methods
