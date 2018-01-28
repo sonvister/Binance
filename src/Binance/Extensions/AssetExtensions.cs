db5e9ccda1fd72f7f6fb4773fce7cc1d277c197a
sonvister <sonvister@gmail.com>
2018-01-28 11:38:26 -0600

    Add Asset extension to validate amount precision
    Add unit test.

---
+++ src/Binance/Extensions/AssetExtensions.cs
@@ -0,0 +1,22 @@
+﻿// ReSharper disable once CheckNamespace
+using System;
+
+namespace Binance
+{
+    public static class AssetExtensions
+    {
+        /// <summary>
+        /// Determine if amount is valid for <see cref="Asset"/>.
+        /// </summary>
+        /// <param name="asset"></param>
+        /// <param name="amount"></param>
+        /// <returns></returns>
+        public static bool IsAmountValid(this Asset asset, decimal amount)
+        {
+            var precision = (decimal)Math.Pow(10, -asset.Precision);
+
+            return amount > 0
+                && amount % precision == 0;
+        }
+    }
+}
---
+++ test/Binance.Tests/AssetTest.cs
@@ -0,0 +1,36 @@
+﻿using Xunit;
+
+namespace Binance.Tests
+{
+    public class AssetTest
+    {
+        [Fact]
+        public void IsAmountValid()
+        {
+            var asset = Asset.BTC;
+
+            Assert.Equal(8, asset.Precision);
+
+            Assert.True(asset.IsAmountValid(100.000000000000m));
+            Assert.True(asset.IsAmountValid(010.000000000000m));
+            Assert.True(asset.IsAmountValid(001.000000000000m));
+            Assert.True(asset.IsAmountValid(000.100000000000m));
+            Assert.True(asset.IsAmountValid(000.010000000000m));
+            Assert.True(asset.IsAmountValid(000.001000000000m));
+            Assert.True(asset.IsAmountValid(000.000100000000m));
+            Assert.True(asset.IsAmountValid(000.000010000000m));
+            Assert.True(asset.IsAmountValid(000.000001000000m));
+            Assert.True(asset.IsAmountValid(000.000000100000m));
+            Assert.True(asset.IsAmountValid(000.000000010000m));
+
+            Assert.False(asset.IsAmountValid(000.000000001000m));
+            Assert.False(asset.IsAmountValid(000.000000000100m));
+            Assert.False(asset.IsAmountValid(000.000000000010m));
+            Assert.False(asset.IsAmountValid(000.000000000001m));
+
+            Assert.False(asset.IsAmountValid(000.000000000000m));
+
+            Assert.False(asset.IsAmountValid(-000.000000010000m));
+        }
+    }
+}
