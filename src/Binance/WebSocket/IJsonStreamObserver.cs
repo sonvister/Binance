3d6e954f7886b9d336bc2136039f90ca167dd2a8
sonvister <sonvister@gmail.com>
2018-02-08 00:30:45 -0600

    Add JSON stream abstractions

---
+++ src/Binance/WebSocket/IJsonStreamObserver.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Binance.WebSocket
+{
+    public interface IJsonStreamObserver
+    {
+        /// <summary>
+        /// Get the observed streams.
+        /// </summary>
+        IEnumerable<string> ObservedStreams { get; }
+
+        /// <summary>
+        /// Handle JSON message events.
+        /// </summary>
+        /// <param name="stream">The stream identifier.</param>
+        /// <param name="json">The JSON data.</param>
+        /// <param name="token">The cancellation token.</param>
+        /// <returns></returns>
+        Task OnMessageAsync(string stream, string json, CancellationToken token = default);
+    }
+}
---
+++ src/Binance/WebSocket/Manager/IJsonStreamController.cs
@@ -0,0 +1,12 @@
+﻿using Binance.Utility;
+
+namespace Binance.WebSocket.Manager
+{
+    public interface IJsonStreamController : ITaskController
+    {
+        /// <summary>
+        /// Get the JSON stream.
+        /// </summary>
+        IJsonStream JsonStream { get; }
+    }
+}
